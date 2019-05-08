82dc1af2333eb5e204cecc367af204a4487b5ca8
Christian Welling <csw73@cornell.edu>
2019-05-08 22:18:39 +0000

    Added setup script for use in udacity workspace

---
+++ git_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to automatically set up the git configuration within the workspace 
+# so you dont have to redo this every time
+git config --global user.name "Christian Welling"
+git config --global user.email "csw73@cornell.edu"
+echo Set up Git Configuration!
