651d28d7d60b8cd0451ea09a00b694e57a559b1d
Christian Welling <csw73@cornell.edu>
2019-05-13 19:06:22 -0700

    changed init for debugging

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -6,30 +6,36 @@ import rospy
 import os
 import cv2
 
+
+USING_CLASSIFIER = False
+
 class TLClassifier(object):
     def __init__(self, is_site):
         #TODO load classifier
 
-        if is_site:
-            self.SSD_GRAPH_FILE_PATH = rospy.get_param('~/site_model_path', "not found")
+        if USING_CLASSIFIER: 
+            if is_site:
+                self.SSD_GRAPH_FILE_PATH = rospy.get_param('~/site_model_path', "not found")
+            else:
+                self.SSD_GRAPH_FILE_PATH = rospy.get_param('~/sim_model_path', "not found")
+
+            rospy.logdebug('#### is_site %s? SSD_GRAPH_FILE_PATH read = %s',is_site, self.SSD_GRAPH_FILE_PATH)
+
+
+            self.detection_graph = self.load_graph(self.SSD_GRAPH_FILE_PATH)
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+
+            # Each box represents a part of the image where a particular object was detected.
+            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+
+            # The classification of the object (integer id).
+            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
         else:
-            self.SSD_GRAPH_FILE_PATH = rospy.get_param('~/sim_model_path', "not found")
-
-        rospy.logdebug('#### is_site %s? SSD_GRAPH_FILE_PATH read = %s',is_site, self.SSD_GRAPH_FILE_PATH)
-
-
-        self.detection_graph = self.load_graph(self.SSD_GRAPH_FILE_PATH)
-        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-
-        # Each box represents a part of the image where a particular object was detected.
-        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-
-        # The classification of the object (integer id).
-        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            pass
 
     def filter_boxes(self,min_score, boxes, scores, classes):
         """Return boxes with a confidence >= `min_score`"""
