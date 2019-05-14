47c92cf8a7ffbc2a733e25e9327170fedd11556c
Christian Welling <csw73@cornell.edu>
2019-05-13 19:01:01 -0700

    Added Ying's TLCassifier class

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -31,15 +31,84 @@ class TLClassifier(object):
         # The classification of the object (integer id).
         self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
 
+    def filter_boxes(self,min_score, boxes, scores, classes):
+        """Return boxes with a confidence >= `min_score`"""
+        n = len(classes)
+        idxs = []
+        for i in range(n):
+            if scores[i] >= min_score:
+                idxs.append(i)
+
+        filtered_boxes = boxes[idxs, ...]
+        filtered_scores = scores[idxs, ...]
+        filtered_classes = classes[idxs, ...]
+        return filtered_boxes, filtered_scores, filtered_classes
+
+    def load_graph(self,graph_file):
+        """Loads a frozen inference graph"""
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        # graph_file_1='models/ssd_sim/frozen_inference_graph.pb'
+        # with open(graph_file_1, 'rb') as f:
+        #     serialized = f.read()
+        #     detector_graph_def = tf.GraphDef()
+        #     detector_graph_def.ParseFromString(serialized)
+        #     tf.import_graph_def(detector_graph_def, name='detector')
+        return graph
+
+
+
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+
+        image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
+        # image_np = np.expand_dims(image, axis=0)
+
+        with tf.Session(graph=self.detection_graph) as sess:
+            # Actual detection.
+            (boxes, scores, classes) = sess.run([self.detection_boxes, self.detection_scores, self.detection_classes],
+                                                feed_dict={self.image_tensor: image_np})
+
+            # Remove unnecessary dimensions
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes)
+
+            confidence_cutoff = 0.8
+            # Filter boxes with a confidence score less than `confidence_cutoff`
+            boxes, scores, classes = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
+            #
+            # # The current box coordinates are normalized to a range between 0 and 1.
+            # # This converts the coordinates actual location on the image.
+            # width, height = image.size
+            # box_coords = to_image_coords(boxes, height, width)
+            #
+            # # Each class with be represented by a differently colored box
+            # draw_boxes(image, box_coords, classes)
+            #
+            # plt.figure(figsize=(12, 8))
+            # plt.imshow(image)
+
+            if len(classes) == 0:
+                return TrafficLight.UNKNOWN
+
+            switcher = {
+                1: TrafficLight.GREEN,
+                2: TrafficLight.RED,
+                3: TrafficLight.YELLOW,
+                }
+
+            class_result = switcher.get(classes[0], TrafficLight.UNKNOWN)
+            rospy.logdebug('####classification class = %d', class_result)
+            return class_result
